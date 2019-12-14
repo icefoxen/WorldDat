f3c612e21c5a5f8053e791c632b7e0ff6fa09930
Simon Heath <icefox@dreamquest.io>
2019-12-14 14:48:22 -0500

    PeerMap seems done-ish, now thinking about protocol.

--- src/proto.rs
+++ src/proto.rs
@@ -21,15 +21,30 @@ pub enum FindPeerRequest {
 }
 
 pub enum FindPeerResponse {
-    FindPeerResponsePeerFound {
-        id: PeerId,
-        addr: SocketAddr,
-    },
-    FindPeerResponsePeerNotFound {
+    /// We know the peer, here it is
+    PeerFound { id: PeerId, addr: SocketAddr },
+    /// We don't know the peer, will send the closest ones we know.
+    PeerNotFound {
         id: PeerId,
         /// It's ok to have this be unbounded size 'cause we only receive a fixed-size
         /// buffer from any client...  I'm pretty sure.
         /// TODO: Verify!
         neighbors: Vec<Contact>,
     },
+}
+
+/// Ask if the destination has the given value
+pub enum FindValueRequest {}
+
+pub enum FindValueResponse {
+    /// We have it, here it is
+    ValueFound {},
+    /// We don't have it, here's peers who...
+    /// TODO: We know have it?  We know of who might have it?
+    ValueNotFound { neighbors: Vec<Contact> },
+}
+
+/// We have this value
+pub enum ValueAnnounce {
+    Value,
 }
